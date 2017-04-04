76a420bb034261beab4aef93ab78e1b9dacef628
SAGAR <SAGAR@SAGAR-PC>
2017-04-04 23:17:12 +0530

    first changes from STS

--- deviosbuddy/src/main/java/devopsbuddy/DeviosbuddyApplication.java
+++ deviosbuddy/src/main/java/devopsbuddy/DeviosbuddyApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeviosbuddyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("This is Devops Application");
 		SpringApplication.run(DeviosbuddyApplication.class, args);
 	}
 }
